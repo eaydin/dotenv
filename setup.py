fa68df53c73c36ada937e648f3d39c31cb97bb67
M. Emre Aydın <emraydin@gmail.com>
2016-11-17 23:23:42 +0300

    setup file added.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='dotenv',
+      version='1.0',
+      description='Read and write .env files',
+      author='M. Emre Aydin',
+      author_email='emraydin@gmail.com',
+      url='https://github.com/eaydin/dotenv',
+      packages=['dotenv']
+      )
