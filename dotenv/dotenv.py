b71478ae7e1be74c2446c057805b215546b8fb4e
M. Emre Aydin <emraydin@gmail.com>
2016-11-18 19:17:07 +0300

    keeps order of data, understands integer key values, and understands comments in same line.

--- dotenv/dotenv.py
+++ dotenv/dotenv.py
@@ -1,6 +1,7 @@
 import shutil
 import tempfile
 import os
+from collections import Counter
 
 
 class Error(Exception):
@@ -46,22 +47,37 @@ def _get_keys_from_line(line):
 
 def _get_value_from_line(line):
     """Parses the values from incoming line"""
+    comment=None
     if '=' not in line:
         value = True
-        return value
+        return value, comment
     else:
+
+        # if ends with comments, take them out
+        # if sameline comments are NOT seperated by a whitespace
+        # they are considered part of the value
+        if " #" in line:
+            comment = "#" + line.split(" #")[1].strip()
+            line = line.split(" #")[0]
+        elif " //" in line:
+            comment = "//" + line.split(" //")[1].strip()
+            line = line.split(" //")[0]
+        elif " ;" in line:
+            comment = ";" + line.split(" ;")[1].strip()
+            line = line.split(" ;")[0]
+
         columns = line.split('=')
 
         if len(columns) == 2:
             if columns[1] == '':
                 value = ''
-                return value
+                return value, comment
             else:
                 value = columns[1]
-                return value.strip()
+                return value.strip(), comment
         else:
             value = '='.join(columns[1:])
-            return value.strip()
+            return value.strip(), comment
 
 
 class Environment(object):
@@ -73,8 +89,13 @@ class Environment(object):
         self.backup_file_name = ''
         self.backup_file()
         self.env = {}
+
+        self.keys = []
+        self.values = []
         self.comments = []
-        self.refresh_env()
+        self.lines = []
+
+        self.read_env()
 
     def backup_file(self):
         try:
@@ -101,48 +122,144 @@ class Environment(object):
         except Exception as err:
             print("Error: {0}".format(str(err)))
 
-    def refresh_env(self):
+    def read_env(self):
         if self.stat:
             lines = self.read_from_file()
             self.parse_env(lines)
 
     def parse_env(self, env_lines):
-        env = {}
 
-        for line in env_lines:
+        for num, line in enumerate(env_lines):
+
             line = line.strip()
             if len(line) > 0:
                 if line[0] == "#" or line[0] == ';' or line[:2] == '//':
                     # this is a commented line
+                    self.lines.append(num)
                     self.comments.append(line)
-                    pass
+                    self.keys.append(None)
+                    self.values.append(None)
                 else:
+                    # we have values here
                     keys = _get_keys_from_line(line)
-                    value = _get_value_from_line(line)
+                    value, comment = _get_value_from_line(line)
 
                     for key in keys:
                         if key != '':
-                            env[key] = value
+                            self.keys.append(key)
+                            self.values.append(value)
+                            self.lines.append(num)
+                            self.comments.append(comment)
 
             else:
                 # empty line
-                pass
-        self.env = env
+                self.keys.append(None)
+                self.values.append(None)
+                self.lines.append(num)
+                self.comments.append(None)
+        self.refresh_env()
+
+    def refresh_env(self):
+        self.env = {}
+        self.env = dict(zip(self.keys, self.values))
+
+    def _refresh_lists(self):
+        for key in self.env:
+            if not key in self.keys:
+                self.keys.append(key)
+                self.values.append(self.env[key])
+                self.lines.append(max(self.lines)+1)
+                self.comments.append(None)
+
+            else:
+                idx = self.keys.index(key)
+                self.values[idx] = self.env[key]
+
+        self._reorganize()
+
+    def _reorganize(self):
+
+        # lines with multiple values
+        mult = [ k for k,v in Counter(self.lines).items() if v > 1 ]
+        if len(mult) > 0:
+            for item in mult:
+                # this is the index number of line element
+                idxs = [i for i,j in enumerate(self.lines) if j == item]
+
+                # now check for values in this index, if they are not same, split them apart
+                idxs = tuple(idxs)
+
+                vals = [i for i in map(self.values.__getitem__, idxs)]
+
+                # if all of them are them same (none changed)
+                if len(Counter(vals)) == 1:
+                    pass
+                else:
+                    # we have multiple values for this single line
+                    # we'll keep the values with highest frequency
+                    # rest will be added to new lines
+                    # --- the above expression is not done yet ----
+                    # instead, we simply divide keys if they are not all equal
+                    for i,j in enumerate(idxs):
+                        # we keep the first line as is
+                        if i != 0:
+                            # we add 1 to each following line numbers
+                            self.lines = self.lines[:j] + [x+1 for x in self.lines[j:]]
+
+    def _create_line(self, line):
+
+        new_line = ""
+
+        # get id of the list for line number
+        idxs = [i for i,j in enumerate(self.lines) if j == line]
+        idxs = tuple(idxs)
+
+        # get keys for those id's which are not None
+        keys = [i for i in map(self.keys.__getitem__, idxs) if i]
+        # get values for those id's which are not None
+        values = [i for i in map(self.values.__getitem__, idxs) if i]
+        # get comments for those id's which are not None
+        comments = [i for i in map(self.comments.__getitem__, idxs) if i]
+
+        if len(keys) == 1:
+            # we have a single key
+            new_line = str(keys[0])
+        elif len(keys) > 1:
+            # we have multiple keys for this line
+            new_line = " ,".join(keys)
+        elif len(keys) == 0:
+            # no keys for this line
+            new_line = ""
+
+        if len(values) > 0:
+            # we have values for this (or these) key(s)
+            new_value = str(values[0])
+            new_line += " = {0}".format(new_value)
+
+        if len(comments) > 0:
+            # add a space if we already have keys
+            if new_line != "":
+                new_line += " "
+            # we take only one of the comments at this point
+            # usually they all end up being the same line
+            new_line += comments[0]
+
+        new_line += "\n"
+
+        return new_line
 
     def update_file(self):
         # print("Backup filename is : {0}".format(self.backup_file_name))
         if self.stat:
             with open(self.file_path, 'w') as fp:
-                if len(self.comments) > 0:
-                    fp.writelines('\n'.join(self.comments))
-                    # Empty line after comments
-                    fp.write("\n\n")
-                for key in self.env:
-                    fp.write("{key} = {value}\n".format(key=key, value=self.env[key]))
+                for line in range(max(self.lines)+1):
+                    fp.write(self._create_line(line))
 
 if __name__ == '__main__':
-    print("Testing to printout")
+    print("Line |  Keys")
+    print("------------------")
     a = Environment()
     if a.stat:
-        for i in a.env:
-            print("{0} = {1}".format(i, a.env[i]))
+        for i in range(max(a.lines)+1):
+            print("{0:4d}    {1}".format(i, a._create_line(i)[:-1]))
+    print("------------------")
