465251cde30a6b455bc19d317783714d982a7c38
M. Emre Aydin <emraydin@gmail.com>
2016-11-18 19:17:20 +0300

    updated version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='dotenv',
-      version='1.1',
+      version='1.2',
       description='Read and write .env files',
       author='M. Emre Aydin',
       author_email='emraydin@gmail.com',
